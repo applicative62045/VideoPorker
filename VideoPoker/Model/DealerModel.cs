38fe03439a8d6b23f2437a8b5515e98dbb75eb17
togakawayaki <applicative62045@gmail.com>
2015-04-26 22:01:08 +0900

    手札同士の強弱判定を実�

--- VideoPoker/Model/DealerModel.cs
+++ VideoPoker/Model/DealerModel.cs
@@ -119,7 +119,6 @@ namespace VideoPoker.Model
 
         public Strength Judge(IEnumerable<CardModel> lhs, IEnumerable<CardModel> rhs)
         {
-            throw new NotImplementedException();
             Strength result;
             var rolel = GetRole(lhs);
             var roler = GetRole(rhs);
@@ -127,17 +126,45 @@ namespace VideoPoker.Model
             if (rolel > roler) {
                 result = Strength.Strong;
             } else if (rolel == roler) {
-                CardModel.CardNumber numberl;
-                CardModel.CardNumber numberr;
-
                 switch (rolel) {
                     case Role.StraightFlush:
                     case Role.Straight:
+                        // 最大の値で比較する、ただし [Five, Four, Three, Two, Ace] の組み合わせはAceが入っているが最弱とする
+                        var numbersl = lhs.Select(v => v.Number);
+                        var numbersr = rhs.Select(v => v.Number);
+                        Func<CardModel.CardNumber, IEnumerable<CardModel.CardNumber>, int> toNativeNumber = 
+                            (x, xs) => x == CardModel.CardNumber.Ace && xs.Contains(CardModel.CardNumber.Two) ? -1 : (int)x;
+                        var maxl = lhs.Select(v => v.Number).Select(v => toNativeNumber(v, numbersl)).Max();
+                        var maxr = rhs.Select(v => v.Number).Select(v => toNativeNumber(v, numbersr)).Max();
+                        if (maxl > maxr)
+                            result = Strength.Strong;
+                        else if (maxl == maxr)
+                            result = Strength.Same;
+                        else
+                            result = Strength.Weak;
+                        break;
+                    default:
+                        // 数字の組を降順並び替え、その数字を射影
+                        var ls = lhs.GroupBy(v => v).OrderBy(v => v.Count()).OrderByDescending(v => v.Key).Select(v => v.First());
+                        var rs = rhs.GroupBy(v => v).OrderBy(v => v.Count()).OrderByDescending(v => v.Key).Select(v => v.First()).ToArray();
+                        // カードが異なった以降をインデックスを付加して取得
+                        var differents = ls.Select((v, i) => new { Index = i, Card = v }).SkipWhile(v => v.Card == rs[v.Index]);
+
+                        if (!differents.Any()) {
+                            result = Strength.Same;
+                        }  else {
+                            var cmpTarget = differents.First();
+                            if (cmpTarget.Card > rs[cmpTarget.Index])
+                                result = Strength.Strong;
+                            else
+                                result = Strength.Weak;
+                        }
                         break;
                 }
             } else {
                 result = Strength.Weak;
             }
+            return result;
         }
     }
 }
--- VideoPorkerTest/DealerModelTest.cs
+++ VideoPorkerTest/DealerModelTest.cs
@@ -131,5 +131,202 @@ namespace VideoPorkerTest
             Assert.IsTrue(Dealer.GetRole(straightFlushRandomOrder) == DealerModel.Role.StraightFlush);
             Assert.IsTrue(Dealer.GetRole(straightFlushWeak) == DealerModel.Role.StraightFlush);
         }
+
+        [TestMethod]
+        public void Judge()
+        {
+            var straightFlush = new List<CardModel>()
+            {
+                new CardModel() {Mark = CardModel.CardMark.Club, Number = CardModel.CardNumber.Six},
+                new CardModel() {Mark = CardModel.CardMark.Club, Number = CardModel.CardNumber.Seven},
+                new CardModel() {Mark = CardModel.CardMark.Club, Number = CardModel.CardNumber.Eight},
+                new CardModel() {Mark = CardModel.CardMark.Club, Number = CardModel.CardNumber.Nine},
+                new CardModel() {Mark = CardModel.CardMark.Club, Number = CardModel.CardNumber.Ten},
+            };
+
+            var fourofAKind = new List<CardModel>()
+            {
+                new CardModel() {Mark = CardModel.CardMark.Dia, Number = CardModel.CardNumber.Nine},
+                new CardModel() {Mark = CardModel.CardMark.Heart, Number = CardModel.CardNumber.Nine},
+                new CardModel() {Mark = CardModel.CardMark.Spade, Number = CardModel.CardNumber.Nine},
+                new CardModel() {Mark = CardModel.CardMark.Club, Number = CardModel.CardNumber.Nine},
+                new CardModel() {Mark = CardModel.CardMark.Dia, Number = CardModel.CardNumber.Seven},
+            };
+
+            var fullHouse = new List<CardModel>()
+            {
+                new CardModel() {Mark = CardModel.CardMark.Dia, Number = CardModel.CardNumber.Ten},
+                new CardModel() {Mark = CardModel.CardMark.Club, Number = CardModel.CardNumber.Ten},
+                new CardModel() {Mark = CardModel.CardMark.Heart, Number = CardModel.CardNumber.Ten},
+                new CardModel() {Mark = CardModel.CardMark.Club, Number = CardModel.CardNumber.Seven},
+                new CardModel() {Mark = CardModel.CardMark.Dia, Number = CardModel.CardNumber.Seven},
+            };
+
+            var flush = new List<CardModel>()
+            {
+                new CardModel() {Mark = CardModel.CardMark.Dia, Number = CardModel.CardNumber.Two},
+                new CardModel() {Mark = CardModel.CardMark.Dia, Number = CardModel.CardNumber.Four},
+                new CardModel() {Mark = CardModel.CardMark.Dia, Number = CardModel.CardNumber.Six},
+                new CardModel() {Mark = CardModel.CardMark.Dia, Number = CardModel.CardNumber.Nine},
+                new CardModel() {Mark = CardModel.CardMark.Dia, Number = CardModel.CardNumber.Ten},
+            };
+
+            var straight = new List<CardModel>()
+            {
+                new CardModel() {Mark = CardModel.CardMark.Spade, Number = CardModel.CardNumber.Two},
+                new CardModel() {Mark = CardModel.CardMark.Club, Number = CardModel.CardNumber.Three},
+                new CardModel() {Mark = CardModel.CardMark.Heart, Number = CardModel.CardNumber.Four},
+                new CardModel() {Mark = CardModel.CardMark.Spade, Number = CardModel.CardNumber.Five},
+                new CardModel() {Mark = CardModel.CardMark.Heart, Number = CardModel.CardNumber.Six},
+            };
+
+            var threeofAKind = new List<CardModel>()
+            {
+                new CardModel() {Mark = CardModel.CardMark.Dia, Number = CardModel.CardNumber.Nine},
+                new CardModel() {Mark = CardModel.CardMark.Spade, Number = CardModel.CardNumber.Nine},
+                new CardModel() {Mark = CardModel.CardMark.Club, Number = CardModel.CardNumber.Nine},
+                new CardModel() {Mark = CardModel.CardMark.Club, Number = CardModel.CardNumber.Seven},
+                new CardModel() {Mark = CardModel.CardMark.Dia, Number = CardModel.CardNumber.Ace},
+            };
+
+            var twoPair = new List<CardModel>()
+            {
+                new CardModel() {Mark = CardModel.CardMark.Club, Number = CardModel.CardNumber.Ten},
+                new CardModel() {Mark = CardModel.CardMark.Dia, Number = CardModel.CardNumber.Ten},
+                new CardModel() {Mark = CardModel.CardMark.Dia, Number = CardModel.CardNumber.Two},
+                new CardModel() {Mark = CardModel.CardMark.Spade, Number = CardModel.CardNumber.Two},
+                new CardModel() {Mark = CardModel.CardMark.Spade, Number = CardModel.CardNumber.Eight},
+            };
+
+            var OnePair = new List<CardModel>()
+            {
+                new CardModel() {Mark = CardModel.CardMark.Spade, Number = CardModel.CardNumber.Five},
+                new CardModel() {Mark = CardModel.CardMark.Club, Number = CardModel.CardNumber.Five},
+                new CardModel() {Mark = CardModel.CardMark.Dia, Number = CardModel.CardNumber.Nine},
+                new CardModel() {Mark = CardModel.CardMark.Spade, Number = CardModel.CardNumber.Six},
+                new CardModel() {Mark = CardModel.CardMark.Heart, Number = CardModel.CardNumber.Eight},
+            };
+
+            var highCards = new List<CardModel>()
+            {
+                new CardModel() {Mark = CardModel.CardMark.Club, Number = CardModel.CardNumber.Ten},
+                new CardModel() {Mark = CardModel.CardMark.Dia, Number = CardModel.CardNumber.Eight},
+                new CardModel() {Mark = CardModel.CardMark.Dia, Number = CardModel.CardNumber.Six},
+                new CardModel() {Mark = CardModel.CardMark.Dia, Number = CardModel.CardNumber.Four},
+                new CardModel() {Mark = CardModel.CardMark.Spade, Number = CardModel.CardNumber.Two},
+            };
+
+            Assert.IsTrue(Dealer.Judge(straightFlush, straightFlush) == DealerModel.Strength.Same);
+            Assert.IsTrue(Dealer.Judge(straightFlush, fourofAKind) == DealerModel.Strength.Strong);
+            Assert.IsTrue(Dealer.Judge(straightFlush, fullHouse) == DealerModel.Strength.Strong);
+            Assert.IsTrue(Dealer.Judge(straightFlush, flush) == DealerModel.Strength.Strong);
+            Assert.IsTrue(Dealer.Judge(straightFlush, threeofAKind) == DealerModel.Strength.Strong);
+            Assert.IsTrue(Dealer.Judge(straightFlush, twoPair) == DealerModel.Strength.Strong);
+            Assert.IsTrue(Dealer.Judge(straightFlush, OnePair) == DealerModel.Strength.Strong);
+            Assert.IsTrue(Dealer.Judge(straightFlush, highCards) == DealerModel.Strength.Strong);
+
+            Assert.IsTrue(Dealer.Judge(fourofAKind, straightFlush) == DealerModel.Strength.Weak);
+            Assert.IsTrue(Dealer.Judge(fourofAKind, fourofAKind) == DealerModel.Strength.Same);
+            Assert.IsTrue(Dealer.Judge(fourofAKind, fullHouse) == DealerModel.Strength.Strong);
+            Assert.IsTrue(Dealer.Judge(fourofAKind, flush) == DealerModel.Strength.Strong);
+            Assert.IsTrue(Dealer.Judge(fourofAKind, threeofAKind) == DealerModel.Strength.Strong);
+            Assert.IsTrue(Dealer.Judge(fourofAKind, twoPair) == DealerModel.Strength.Strong);
+            Assert.IsTrue(Dealer.Judge(fourofAKind, OnePair) == DealerModel.Strength.Strong);
+            Assert.IsTrue(Dealer.Judge(fourofAKind, highCards) == DealerModel.Strength.Strong);
+
+            Assert.IsTrue(Dealer.Judge(fullHouse, straightFlush) == DealerModel.Strength.Weak);
+            Assert.IsTrue(Dealer.Judge(fullHouse, fourofAKind) == DealerModel.Strength.Weak);
+            Assert.IsTrue(Dealer.Judge(fullHouse, fullHouse) == DealerModel.Strength.Same);
+            Assert.IsTrue(Dealer.Judge(fullHouse, flush) == DealerModel.Strength.Strong);
+            Assert.IsTrue(Dealer.Judge(fullHouse, threeofAKind) == DealerModel.Strength.Strong);
+            Assert.IsTrue(Dealer.Judge(fullHouse, twoPair) == DealerModel.Strength.Strong);
+            Assert.IsTrue(Dealer.Judge(fullHouse, OnePair) == DealerModel.Strength.Strong);
+            Assert.IsTrue(Dealer.Judge(fullHouse, highCards) == DealerModel.Strength.Strong);
+
+            Assert.IsTrue(Dealer.Judge(flush, straightFlush) == DealerModel.Strength.Weak);
+            Assert.IsTrue(Dealer.Judge(flush, fourofAKind) == DealerModel.Strength.Weak);
+            Assert.IsTrue(Dealer.Judge(flush, fullHouse) == DealerModel.Strength.Weak);
+            Assert.IsTrue(Dealer.Judge(flush, flush) == DealerModel.Strength.Same);
+            Assert.IsTrue(Dealer.Judge(flush, threeofAKind) == DealerModel.Strength.Strong);
+            Assert.IsTrue(Dealer.Judge(flush, twoPair) == DealerModel.Strength.Strong);
+            Assert.IsTrue(Dealer.Judge(flush, OnePair) == DealerModel.Strength.Strong);
+            Assert.IsTrue(Dealer.Judge(flush, highCards) == DealerModel.Strength.Strong);
+
+            Assert.IsTrue(Dealer.Judge(threeofAKind, straightFlush) == DealerModel.Strength.Weak);
+            Assert.IsTrue(Dealer.Judge(threeofAKind, fourofAKind) == DealerModel.Strength.Weak);
+            Assert.IsTrue(Dealer.Judge(threeofAKind, fullHouse) == DealerModel.Strength.Weak);
+            Assert.IsTrue(Dealer.Judge(threeofAKind, flush) == DealerModel.Strength.Weak);
+            Assert.IsTrue(Dealer.Judge(threeofAKind, threeofAKind) == DealerModel.Strength.Same);
+            Assert.IsTrue(Dealer.Judge(threeofAKind, twoPair) == DealerModel.Strength.Strong);
+            Assert.IsTrue(Dealer.Judge(threeofAKind, OnePair) == DealerModel.Strength.Strong);
+            Assert.IsTrue(Dealer.Judge(threeofAKind, highCards) == DealerModel.Strength.Strong);
+
+            Assert.IsTrue(Dealer.Judge(twoPair, straightFlush) == DealerModel.Strength.Weak);
+            Assert.IsTrue(Dealer.Judge(twoPair, fourofAKind) == DealerModel.Strength.Weak);
+            Assert.IsTrue(Dealer.Judge(twoPair, fullHouse) == DealerModel.Strength.Weak);
+            Assert.IsTrue(Dealer.Judge(twoPair, flush) == DealerModel.Strength.Weak);
+            Assert.IsTrue(Dealer.Judge(twoPair, threeofAKind) == DealerModel.Strength.Weak);
+            Assert.IsTrue(Dealer.Judge(twoPair, twoPair) == DealerModel.Strength.Same);
+            Assert.IsTrue(Dealer.Judge(twoPair, OnePair) == DealerModel.Strength.Strong);
+            Assert.IsTrue(Dealer.Judge(twoPair, highCards) == DealerModel.Strength.Strong);
+
+            Assert.IsTrue(Dealer.Judge(OnePair, straightFlush) == DealerModel.Strength.Weak);
+            Assert.IsTrue(Dealer.Judge(OnePair, fourofAKind) == DealerModel.Strength.Weak);
+            Assert.IsTrue(Dealer.Judge(OnePair, fullHouse) == DealerModel.Strength.Weak);
+            Assert.IsTrue(Dealer.Judge(OnePair, flush) == DealerModel.Strength.Weak);
+            Assert.IsTrue(Dealer.Judge(OnePair, threeofAKind) == DealerModel.Strength.Weak);
+            Assert.IsTrue(Dealer.Judge(OnePair, twoPair) == DealerModel.Strength.Weak);
+            Assert.IsTrue(Dealer.Judge(OnePair, OnePair) == DealerModel.Strength.Same);
+            Assert.IsTrue(Dealer.Judge(OnePair, highCards) == DealerModel.Strength.Strong);
+
+            Assert.IsTrue(Dealer.Judge(highCards, straightFlush) == DealerModel.Strength.Weak);
+            Assert.IsTrue(Dealer.Judge(highCards, fourofAKind) == DealerModel.Strength.Weak);
+            Assert.IsTrue(Dealer.Judge(highCards, fullHouse) == DealerModel.Strength.Weak);
+            Assert.IsTrue(Dealer.Judge(highCards, flush) == DealerModel.Strength.Weak);
+            Assert.IsTrue(Dealer.Judge(highCards, threeofAKind) == DealerModel.Strength.Weak);
+            Assert.IsTrue(Dealer.Judge(highCards, twoPair) == DealerModel.Strength.Weak);
+            Assert.IsTrue(Dealer.Judge(highCards, OnePair) == DealerModel.Strength.Weak);
+            Assert.IsTrue(Dealer.Judge(highCards, highCards) == DealerModel.Strength.Same);
+
+            var weakStraightFlush = new List<CardModel>()
+            {
+                new CardModel() {Mark = CardModel.CardMark.Club, Number = CardModel.CardNumber.Five},
+                new CardModel() {Mark = CardModel.CardMark.Club, Number = CardModel.CardNumber.Four},
+                new CardModel() {Mark = CardModel.CardMark.Club, Number = CardModel.CardNumber.Three},
+                new CardModel() {Mark = CardModel.CardMark.Club, Number = CardModel.CardNumber.Two},
+                new CardModel() {Mark = CardModel.CardMark.Club, Number = CardModel.CardNumber.Ace},
+            };
+            Assert.IsTrue(Dealer.Judge(straightFlush, weakStraightFlush) == DealerModel.Strength.Strong);
+
+            var sixStraightFlush = new List<CardModel>()
+            {
+                new CardModel() {Mark = CardModel.CardMark.Club, Number = CardModel.CardNumber.Six},
+                new CardModel() {Mark = CardModel.CardMark.Club, Number = CardModel.CardNumber.Five},
+                new CardModel() {Mark = CardModel.CardMark.Club, Number = CardModel.CardNumber.Four},
+                new CardModel() {Mark = CardModel.CardMark.Club, Number = CardModel.CardNumber.Three},
+                new CardModel() {Mark = CardModel.CardMark.Club, Number = CardModel.CardNumber.Two},
+            };
+            Assert.IsTrue(Dealer.Judge(sixStraightFlush, weakStraightFlush) == DealerModel.Strength.Strong);
+            
+            var sixStraighFlush2 = new List<CardModel>()
+            {
+                sixStraightFlush[1],
+                sixStraightFlush[2],
+                sixStraightFlush[3],
+                sixStraightFlush[4],
+                sixStraightFlush[0],
+            };
+            Assert.IsTrue(Dealer.Judge(sixStraightFlush, sixStraighFlush2) == DealerModel.Strength.Same);
+            
+            var onePair2 = new List<CardModel>()
+            {
+                OnePair[2],
+                OnePair[3],
+                OnePair[0],
+                OnePair[1],
+                new CardModel() {Mark = CardModel.CardMark.Spade, Number = CardModel.CardNumber.Jack},
+            };
+            Assert.IsTrue(Dealer.Judge(OnePair, onePair2) == DealerModel.Strength.Weak);
+        }
     }
 }
